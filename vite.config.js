e18e591c1bae1efeb8fd33910d28b44a75b757d9
cuizihao <cuizihao@sutpc.com>
2024-11-11 14:23:25 +0800

    feat: 去掉screen路径

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,27 @@
 import events from 'events';
-events.EventEmitter.defaultMaxListeners = 20; // 设置监听器数量
+events.EventEmitter.defaultMaxListeners = 20;
 import { defineConfig } from 'vite';
 import path from 'path';
-export default defineConfig({
-  root: './',
-  base: '/screen',
-  build: {
-    outDir: 'screen',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        anotherPage: 'demo.html', // 如果有多个入口页面
+
+export default defineConfig(({ mode }) => {
+  // 根据模式来判断 base 和 outDir
+  const isScreenMode = mode === 'screen';
+  return {
+    root: './',
+    base: isScreenMode ? '/screen' : '/',
+    build: {
+      outDir: isScreenMode ? 'screen' : 'dist',
+      rollupOptions: {
+        input: {
+          main: 'index.html',
+          anotherPage: 'demo.html', // 如果有多个入口页面
+        },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 文件夹
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'), // 配置 @ 指向 src 文件夹
+      },
     },
-  },
+  };
 });
